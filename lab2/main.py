39627855c8820d41647f6011eb5220b29dab79c1
Mateusz Obrzut <56389019+matt628@users.noreply.github.com>
2020-10-29 00:22:48 +0100

    Update main.py

--- lab2/main.py
+++ lab2/main.py
@@ -1,5 +1,6 @@
 import runpy
 def main():
+    # jeżeli nie działa należy odpalić każdy skryp c_.py komendą python _ścieżka_do_pliku_
     runpy.run_path(filepath='c1.py')
     runpy.run_path(filepath='c2.py')
     runpy.run_path(filepath='c3.py')
