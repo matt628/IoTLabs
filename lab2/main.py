962da8f2325e2feef674a838d5aaeca94db16beb
Mateusz Obrzut <56389019+matt628@users.noreply.github.com>
2020-10-29 00:18:22 +0100

    Update main.py

--- lab2/main.py
+++ lab2/main.py
@@ -3,3 +3,5 @@ def main():
     runpy.run_path(filepath='c1.py')
     runpy.run_path(filepath='c2.py')
     runpy.run_path(filepath='c3.py')
+    runpy.run_path(filepath='c4.py')
+    runpy.run_path(filepath='c5.py')
